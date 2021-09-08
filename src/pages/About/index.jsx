cb1385d628bd44d494f260269246b560e1605975
StevenBLR_PC <s.belair95@gmail.com>
2021-09-08 20:32:42 +0200

    wip about page

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -1,8 +1,18 @@
 import { Component } from 'react';
+import Banner from '../../components/Banner';
+import bannerImg from '../../assets/about_bannerImg.png';
+import Dropdown from '../../components/Dropdown';
 
 export class About extends Component {
     render() {
-        return <div></div>;
+        const fiabilityTxt =
+            'La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.';
+        return (
+            <div>
+                <Banner image={bannerImg} />
+                <Dropdown title="Fiabilité" content={fiabilityTxt} />
+            </div>
+        );
     }
 }
 
